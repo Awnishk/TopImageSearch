6fd03fedea08e3c5c350a8502920f309239e56c3
AWNISH KUMAR <awnish.cse@gmail.com>
2019-09-17 11:23:03 +0530

    Update GalleryActivity.kt
    Exception handling

--- app/src/main/java/com/attra/searchimgurimage/view/GalleryActivity.kt
+++ app/src/main/java/com/attra/searchimgurimage/view/GalleryActivity.kt
@@ -1,9 +1,10 @@
-package com.attra.myimage.view
+package com.attra.searchimgurimage.view
 
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.attra.myimage.adapter.GalleryAdapter
@@ -28,14 +29,15 @@ class GalleryActivity : AppCompatActivity() {
         searchButton.setOnClickListener(View.OnClickListener {
             val searchText: String = searchImages.text.toString()
             val apiService = ApiService()
-            val context = this as Context;
+            val context = this as Context
             recyclerView.layoutManager = LinearLayoutManager(context)
             GlobalScope.launch(Dispatchers.Main) {
 
                 val apiResponse = apiService.getImageData(searchText).await()
+                System.out.println("Api resp....." + apiResponse.data.size)
 
-                if (apiResponse.data.equals(null) || apiResponse.data.size.equals("0")) {
-                    errorMessgae.visibility= View.VISIBLE
+                if (apiResponse.data.equals(null) || apiResponse.data.size == 0) {
+                    Toast.makeText(context, "No Result Found", Toast.LENGTH_LONG).show()
                 }
                 when (recyclerView != null && apiResponse.status == 200) {
                     true -> recyclerView.adapter = GalleryAdapter(context, apiResponse.data)
